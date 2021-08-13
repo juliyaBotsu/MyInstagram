abd30a6a9ef9f75490963b09e17da835fd33ce0d
yuliaBotsu <juliya.botsu@icloud.com>
2021-08-13 11:01:12 +0300

    fix

--- src/main/java/com/repository/CommentRepository.java
+++ src/main/java/com/repository/CommentRepository.java
@@ -1,15 +1,17 @@
-package com.company.julia.repository;
+package com.repository;
 
-import com.company.julia.entity.Comment;
-import com.company.julia.entity.Post;
+import com.entity.Comment;
+import com.entity.Post;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-
+@Qualifier("commentRepository")
+public interface CommentRepository extends JpaRepository<Comment,Long> {
     List<Comment> findAllByPost(Post post);
+
     Comment findByIdAndUserId(Long commentId, Long userId);
 }
